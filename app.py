3dc6d8b2224c45c6ceacdd018460afeebb64c74f
mr_destructive <gormeet711@gmail.com>
2022-10-28 23:10:42 +0530

    init

---
+++ app.py
@@ -0,0 +1,20 @@
+import frontmatter
+import markdown
+import os 
+from pathlib import Path
+
+post_path = Path("pages")
+output_path = os.path.abspath("pyites")
+
+
+onlyfiles = os.listdir(post_path)
+for file in onlyfiles:
+    with open(Path(post_path) / file, "r") as f:
+        content = f.read()
+        post = frontmatter.loads(content)
+        html_content = markdown.markdown(post.content)
+        if not os.path.exists(output_path):
+            os.mkdir(output_path)
+        title = str(post.get("title", "pykyll-test")) + ".html"
+        with open(Path(output_path) / title, 'w') as f:
+            f.write(html_content)
---
+++ pages/mark.md
@@ -0,0 +1,9 @@
+---
+title: "hello world"
+description: "a sample for pykyll"
+---
+
+hello 
+world
+mark
+file
