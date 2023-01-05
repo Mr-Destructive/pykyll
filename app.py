1b00f885574133007db99c9f34ea7bcbe142a071
mr_destructive <gormeet711@gmail.com>
2023-01-05 20:13:49 +0530

    chore: refactor for loading and output pages

--- app.py
+++ app.py
@@ -19,26 +19,32 @@ def runserver(path: str):
         except KeyboardInterrupt as _:
             httpd.server_close()
 
-
-def main():
-    post_path = Path("pages")
-    output_path = os.path.abspath("pyites")
-
-    onlyfiles = os.listdir(post_path)
+def load_pages(pages_path, output_dir):
+    onlyfiles = os.listdir(pages_path)
+    pages = []
     for file in onlyfiles:
-        with open(Path(post_path) / file, "r") as f:
+        with open(Path(pages_path) / file, "r") as f:
             content = f.read()
             post = frontmatter.loads(content)
             html_content = markdown.markdown(post.content)
-            if not os.path.exists(output_path):
-                os.mkdir(output_path)
-            title = str(post.get("title", "pykyll-test")) + ".html"
-
-            with open(Path(output_path) / "index.html", "a") as f:
+            if not os.path.exists(output_dir):
+                os.mkdir(output_dir)
+            default_title = file.split(".")[0]
+            title = str(post.get("title", default_title)) + ".html"
+            post["title"] = title
+            pages.append(post)
+            with open(Path(output_dir) / "index.html", "a") as f:
                 f.write(f"<li><a href='#'>{post.get('title', 'test')}</a></li>")
-            with open(Path(output_path) / title, "w") as f:
+            with open(Path(output_dir) / title, "w") as f:
                 f.write(html_content)
-    runserver(output_path)
+    return pages
 
+
+def main():
+    pages_dir = Path("pages")
+    output_dir = os.path.abspath("pyites")
+
+    load_pages(pages_dir, output_dir)
+    runserver(output_dir)
 
 main()
